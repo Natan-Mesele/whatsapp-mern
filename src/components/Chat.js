d0119083608edb2a6925e9cd24bba577b04d7c4b
Natan-Mesele <huluale12@gmail.com>
2024-01-03 10:05:56 +0300

    n

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,10 +11,11 @@ import MicIcon from '@material-ui/icons/Mic';
 function Chat() {
   return (
     <div className='chat'>
+      
        <div className='chat__header'>
         <Avatar />
         <div className='chat__headerinfo'>
-          <h3>Room name</h3>
+          <h3>nathan mesele</h3>
           <p>Last seen at...</p>
         </div>
         <div className='chat__headerRight'>
